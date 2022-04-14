2d4fa6e9389190330180ddb5b6904744ca207dee
Alexander Katrompas <alexander.katrompas@austincc.edu>
2022-04-13 20:38:12 -0500

    updated get_extension to account for no extension

--- functions.py
+++ functions.py
@@ -7,15 +7,17 @@
 # libraries, and slicing, however this demonstrates linear search
 def get_extension(name):
     dot = -1
+    ext = ""
     # first make sure we have a valid string
     if name and type(name) == str:
-        # now loop through the file backwards looking for the last '.'
+        # now loop through the filename backwards looking for the last '.'
         i = len(name)-1
         while(i >= 0 and dot == -1):
             if name[i] == '.':
                 dot = i
             i -= 1
-    return name[dot:]
+    if dot != -1:
+        ext = name[dot:]
 
 # simple linear search to find number of spaces in a string. there are
 # better ways to do this however this demonstrates linear search
@@ -44,5 +46,4 @@ def print_file(fin):
         print(str(i) + ": " + line[:-1] + " [spaces = " +str(count_spaces(line)) + "]")
         line = fin.readline()
         i += 1
-
 
